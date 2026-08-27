7354487842c166f674b73286e32b3d3efcc3e647
bemndy <brandn.martinez5@gmail.com>
2026-08-27 00:12:58 -0400

    docs: bookkeeping and reorganization (neetcode keeps track of problems), utitlity repos are made for dsa, maybe git submodules later, but right now too complex for that

--- arrays-hasing/217.contains-duplicates.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        s = set()
-        for num in nums:
-            if num not in s:
-                s.add(num)
-            else:
-                return True
-        return False
